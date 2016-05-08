00cb2788aa94254e73dc4b3147e0c7b944656e1b
mccd-jenkins <int-mccd_jenkins@schubergphilis.com>
2016-05-08 22:46:51 +0200

    New release version 5.0.1.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "5.0.1.4-SNAPSHOT"
+VERSION = "5.0.1.4"
 
 setup(name="Marvin",
       version=VERSION,
