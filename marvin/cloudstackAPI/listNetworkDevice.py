b5c2bbed7e889d8f1c7e1e3fca4c5801dae6e839
mccd-jenkins <int-mccd_jenkins@schubergphilis.com>
2016-05-19 11:15:30 +0200

    Update all API files

--- marvin/cloudstackAPI/listNetworkDevice.py
+++ marvin/cloudstackAPI/listNetworkDevice.py
@@ -11,7 +11,7 @@ class listNetworkDeviceCmd (baseCmd):
         """parameters for network device"""
         self.networkdeviceparameterlist = []
         self.typeInfo['networkdeviceparameterlist'] = 'map'
-        """Network device type, now supports ExternalDhcp, PxeServer, F5BigIpLoadBalancer, JuniperSRXFirewall, PaloAltoFirewall"""
+        """Network device type, now supports ExternalDhcp, PxeServer, JuniperSRXFirewall, PaloAltoFirewall"""
         self.networkdevicetype = None
         self.typeInfo['networkdevicetype'] = 'string'
         """"""
